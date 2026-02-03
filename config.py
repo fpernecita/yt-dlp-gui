160d58857a09417601af88f095f84f517a403b79
fpernecita <fperne01@bbk.ac.uk>
2026-02-03 14:49:17 +0000

    assigned a default audio format in config.py

--- config.py
+++ config.py
@@ -7,5 +7,8 @@ import os
 # Default download directory (change path as desired)
 DEFAULT_DOWNLOAD_PATH = os.path.join(os.path.expanduse("~"), "Downloads", "yt-dlp")
 
+#
 FFMPEG_PATH = r"C:\ytdl\ffmpeg.exe" # location of the ffmpeg script
 
+# default audio format
+DEFAULT_AUDIO_FORMAT = "mp3"
