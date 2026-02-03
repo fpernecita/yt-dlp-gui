c482e22098c9f29350db2605e808806e0b037d92
fpernecita <fperne01@bbk.ac.uk>
2026-02-03 15:06:08 +0000

    initialise default callback

--- downloader.py
+++ downloader.py
@@ -7,5 +7,6 @@ from config import DEFAULT_DOWNLOAD_PATH, FFMPEG_PATH, DEFAULT_AUDIO_FORMAT,
 
 class Downloader:
     def __init__(self, download_path=None):
-
+        # downloader with custom download path
         self.download_path == download_path or DEFAULT_DOWNLOAD_PATH
+        self.progress_callback = None
