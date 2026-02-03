b1ac10b088535fe0f20b126186851decc1720f45
fpernecita <fperne01@bbk.ac.uk>
2026-02-03 15:04:57 +0000

    initialised downloader with an additional custom dl path

--- downloader.py
+++ downloader.py
@@ -5,3 +5,7 @@ Downloader module
 import yt-dlp
 from config import DEFAULT_DOWNLOAD_PATH, FFMPEG_PATH, DEFAULT_AUDIO_FORMAT, 
 
+class Downloader:
+    def __init__(self, download_path=None):
+
+        self.download_path == download_path or DEFAULT_DOWNLOAD_PATH
