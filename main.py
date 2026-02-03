1792c47429add6a46d554d71dd5a94ad255ca662
fpernecita <fperne01@bbk.ac.uk>
2026-02-03 16:02:35 +0000

    declared functions in main.py

--- main.py
+++ main.py
@@ -4,4 +4,19 @@ import threading
 from downloader import Downloader
 from config import DEFAULT_DOWNLOAD_PATH
 
+class YtDlpGUI:
+    def __init__(self, root):
 
+    def create_widgets():
+        
+    def browse_folder():
+    
+    def update_progress():
+
+    def start_download(self):
+
+
+def main():
+    
+if __name__ == "__main__":
+    main()
